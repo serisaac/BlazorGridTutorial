409b7c44d12b37efda590eacd668b1fa72780009
sergio isaac moreno alvarez <serisaac11@gmail.com>
2025-08-26 11:52:13 -0700

    Agregar archivos de proyecto.

---
+++ BlazorGridTutorial/Data/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorGridTutorial.Data
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateOnly HireDate { get; set; }
+    }
+}
